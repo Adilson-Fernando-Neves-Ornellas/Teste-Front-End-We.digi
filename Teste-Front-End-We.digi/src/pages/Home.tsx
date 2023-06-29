4f1e2faf5a8511c654283748e6951cefd2192d01
Adilson Fernando <ornellas00@hotmail.com>
2023-06-29 15:33:28 -0300

    feat: organizando os arquivos e as pastas para começar o projeto

---
+++ Teste-Front-End-We.digi/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/header/header';
+import Main from '../components/main/Main';
+import Footer from '../components/footer/footer';
+
+function Home() {
+  return (
+    <>
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
+    </>
+  );
+}
+
+export default Home;
