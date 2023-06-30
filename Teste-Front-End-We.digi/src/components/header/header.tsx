5a2562f06132f66946cdd0409bc99c47820e1d36
Adilson Fernando <ornellas00@hotmail.com>
2023-06-29 22:14:39 -0300

    feat: header estatico e responsivo pronto

--- Teste-Front-End-We.digi/src/components/header/header.tsx
+++ Teste-Front-End-We.digi/src/components/header/header.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import "./header.scss"
+import imgcarrinho from './assets/imgcarrinho.svg'
+import imgmenu from './assets/imgMenu.svg'
+import logoheader from './assets/logoheader.svg'
 
 function Header() {
   return (
-    <></>
+    <header className='header'>
+      <div className='headermenu'>
+          <button className='menubutton'>
+            <img className='buttonimg' src={imgmenu} alt="Imagem Do menu" />
+          </button>
+      </div>
+
+      <a className='headerlink' href="/">
+        <img className='logoheader' src={logoheader} alt="Logo Header We.digi" />
+      </a>
+
+      <div className='headercarinho'>
+        <button className='buttoncarrinho'>
+            <img className='imgcarrinho' src={imgcarrinho} alt="Imagem do carrinho de compra " />
+        </button>
+      </div>
+    </header>
   );
 }
 
