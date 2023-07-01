1b920f431b8db80d2bfb2479fa9c0408cf78d868
Adilson Fernando <ornellas00@hotmail.com>
2023-07-01 00:40:25 -0300

    feat: adicionando a funcionalidade de editar o item da lista

--- Teste-Front-End-We.digi/src/components/main/sublista/sublista.tsx
+++ Teste-Front-End-We.digi/src/components/main/sublista/sublista.tsx
@@ -1,50 +1,91 @@
-import React, {FormEvent, useEffect, useState} from 'react';
-import {v4 as uuidV4} from 'uuid';
-import "./sublista.scss";
+import React, { FormEvent, useEffect, useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import './sublista.scss';
 
-type subListaProps ={
-    id: string;
-    titulo:string;
+type subListaProps = {
+  id: string;
+  titulo: string;
 }[];
 
 function Sublista() {
-  const dataLocalStorage = JSON.parse(localStorage.getItem('subListas')||'[]');
-  const [subLista,setsubLista] = useState<subListaProps>(dataLocalStorage);
-  const [novaSubListaInicial,setNovaSubListaInicial] = useState('');
+  const dataLocalStorage = JSON.parse(localStorage.getItem('subListas') || '[]');
+  const [subLista, setsubLista] = useState<subListaProps>(dataLocalStorage);
+  const [novaSubListaInicial, setNovaSubListaInicial] = useState('');
+  const [idEditado, setIdEditado] = useState<string>(''); 
+  const [tituloEditado, setTituloEditado] = useState<string>(''); 
 
-  function addNovoItem (event: FormEvent){
+  function addNovoItem(event: FormEvent) {
     event.preventDefault();
-    setsubLista([...subLista, { id: uuidV4(), titulo:novaSubListaInicial}]);
+    setsubLista([...subLista, { id: uuidV4(), titulo: novaSubListaInicial }]);
     setNovaSubListaInicial('');
   }
-  function removeItem (id:string){
-    setsubLista(subLista.filter(subLista=>subLista.id !== id))
+
+  function editarItem(id: string) {
+    const itemEditado = subLista.find(item => item.id === id);
+    if (itemEditado) {
+      setTituloEditado(itemEditado.titulo);
+      setIdEditado(id);
+    } else {
+      setIdEditado('');
+      setTituloEditado('');
+    }
   }
-  useEffect(()=> {
+
+  function salvarEdicao(id: string) {
+    setsubLista(subLista.map(item => {
+      if (item.id === id) {
+        return { ...item, titulo: tituloEditado };
+      }
+      return item;
+    }));
+    setIdEditado('');
+    setTituloEditado('');
+  }
+
+  function removeItem(id: string) {
+    setsubLista(subLista.filter(subLista => subLista.id !== id));
+  }
+
+  useEffect(() => {
     localStorage.setItem('subLista', JSON.stringify(subLista));
-  },[subLista])
+  }, [subLista]);
 
   return (
     <div>
-
-      <input 
-       value={novaSubListaInicial}
-       type="text" 
-       placeholder='Adicione sub-itens a sua lista' 
-       onChange={(event) => setNovaSubListaInicial(event.target.value)}
-       />
+      <input
+        value={novaSubListaInicial}
+        type="text"
+        placeholder="Adicione sub-itens a sua lista"
+        onChange={event => setNovaSubListaInicial(event.target.value)}
+      />
 
       <button onClick={addNovoItem}>Adicionar</button>
 
       <ul>
-        {subLista.map(subListasInicial=>(
-        <li key={subListasInicial.id}>{subListasInicial.titulo}
-        <button onClick={() => removeItem(subListasInicial.id)}>Deletar</button>
-        </li>))}
+        {subLista.map(subListasInicial => (
+          <li key={subListasInicial.id}>
+            {subListasInicial.id === idEditado ? (
+              <>
+                <input
+                  type="text"
+                  value={tituloEditado}
+                  onChange={event => setTituloEditado(event.target.value)}
+                />
+                <button onClick={() => salvarEdicao(subListasInicial.id)}>Salvar</button>
+              </>
+            ) : (
+              <>
+                {subListasInicial.titulo}
+                <button onClick={() => editarItem(subListasInicial.id)}>Editar</button>
+              </>
+            )}
+            <button onClick={() => removeItem(subListasInicial.id)}>Deletar</button>
+          </li>
+        ))}
       </ul>
-
     </div>
   );
 }
 
 export default Sublista;
+
